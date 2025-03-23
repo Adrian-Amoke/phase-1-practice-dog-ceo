d3c2d57ac5aa4b0896228c0180c3e15fd122e6f4
Adrian <adrian.amoke@gmail.com>
2025-03-23 15:46:28 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+    loadImages();
+    loadBreeds();
+});
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => {
+        json.message.forEach(image => addImage(image))
+    })
+}
+
